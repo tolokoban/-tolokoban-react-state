fd163c2b3593cbb8d59110c23045a87f6b362a7c
Tolokoban <contact@tolokoban.org>
2023-06-23 15:18:03 +0200

    First commit

---
+++ src/index.ts
@@ -0,0 +1,4 @@
+import { AtomicStateOptions, AtomicStateStorageOptions } from "./atomic-state"
+
+export { default } from "./atomic-state"
+export type { AtomicStateOptions, AtomicStateStorageOptions }
