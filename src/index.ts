d47b9b4b0b236f207ffff11d352ac9f45fe182ae
Tolokoban <contact@tolokoban.org>
2025-12-07 12:18:53 +0100

    Removed Rollup

--- src/index.ts
+++ src/index.ts
@@ -2,3 +2,6 @@ import { AtomicStateOptions, AtomicStateStorageOptions } from "./atomic-state"
 
 export { default } from "./atomic-state"
 export type { AtomicStateOptions, AtomicStateStorageOptions }
+
+export * from "./lang"
+export type { Translation } from "./lang"
